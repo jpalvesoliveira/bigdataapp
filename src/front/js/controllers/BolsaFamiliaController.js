125d68c0ff982e75ddd226a7269855f4db15836f
controllersystems <rodrigo.cruz@outlook.com>
2018-03-21 18:00:14 -0300

    Update index.html e Controller

--- src/front/js/controllers/BolsaFamiliaController.js
+++ src/front/js/controllers/BolsaFamiliaController.js
@@ -1,17 +1,14 @@
-angular.module('bigDataApp').controller('BolsaFamiliaController', function ($scope) {
+angular.module('bigDataApp').controller('BolsaFamiliaController', ['$scope','$http', function($scope,$http) {
 	$scope.pesquisar = function (ano, mes){
 		console.log(ano);
 		console.log(mes);
-		$scope.dados = [
-		  ['Par | Ímpar', '%'],
-		  ['6P 0I', 1.186],
-          ['5P 1I', 8.539],
-          ['4P 2I', 23.812],
-          ['3P 3I', 32.925],
-          ['2P 4I', 23.812],
-          ['1P 5I', 8.539],
-		  ['0P 6I', 1.186]
-		];
-		console.log($scope.dados);
+		
+		$scope.dados = [];
+		
+		$http.get("http://localhost:5260/api/chamado?ano="+ano+"&mes="+mes+"").success(function (data, status){
+			console.log("sucesso rest");
+			console.log(data);
+			drawChartDoacoes();
+		});
 	};
-});
+}]);
